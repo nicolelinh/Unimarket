2d5c503238e8f213c3c0592474e22f6c7a9b2e09
leilani-cardenas <72056787+leilani-cardenas@users.noreply.github.com>
2023-01-31 14:09:29 -0800

    navbar changes from main

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,3 +1,65 @@
+// import React, { Component } from "react";
+// import './navbar.css';
+
+// class Navbar extends Component {
+//     //render is needed BECAUSE it extends from Component
+//     // render inside the parenthesis you write javascript logic to decide what to show
+//     render(){
+//         return(
+            
+//             <nav className="navbar navbar-expand-sm navbar-dark bg-green">
+//                 <div className="container">
+//                     <a className="navbar-brand" href="/"><b>UniMarket</b></a>
+                    
+//                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+//                         <span className="navbar-toggler-icon"></span>
+//                     </button>
+
+//                     <div className="collapse navbar-collapse" id="navbarNav">
+//                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+//                             <li className="nav-item">
+//                                 <a className="nav-link" href="/">following</a>
+//                             </li>
+//                             <li className="nav-item">
+//                                 <a className="nav-link" href="/login">messages</a>
+//                             </li>
+//                             <li className="nav-item">
+//                                 <a className="nav-link" href="/signup">profile</a>
+//                             </li>
+//                             <li className="nav-item">
+//                                 <a className="nav-link" href="/signup">history</a>
+//                             </li>
+//                         </ul>
+//                         <a href="/">#</a>
+//                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+//                             <li className="nav-item">
+//                                 <a className="nav-link" href="/">market</a>
+//                             </li>
+//                             <li className="nav-item">
+//                                 <a className="nav-link" href="/login">carpool</a>
+//                             </li>
+//                             <li className="nav-item">
+//                                 <a className="nav-link" href="/signup">services</a>
+//                             </li>
+//                             <li className="nav-item">
+//                                 <a className="nav-link" href="/signup">sign out</a>
+//                             </li>
+//                             <li className="nav-item">
+//                                 <a className="nav-link" href="/signout">Sign Out</a>
+//                             </li>
+//                             <li>
+//                                 <a className="nav-link" href="/landing">Landing</a>
+//                             </li>
+//                         </ul>
+//                     </div>
+//                 </div>
+//             </nav>
+            
+//         );
+//     }
+// }
+
+// export default Navbar;
 import React, { Component } from "react";
 import './navbar.css';
 
@@ -18,31 +80,13 @@ class Navbar extends Component {
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link" href="/">following</a>
+                                <a className="nav-link" href="/">Home</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/login">messages</a>
+                                <a className="nav-link" href="/login">Login</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/signup">profile</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/signup">history</a>
-                            </li>
-                        </ul>
-                        <a href="/">#</a>
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link" href="/">market</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/login">carpool</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/signup">services</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/signup">sign out</a>
+                                <a className="nav-link" href="/signup">Sign Up</a>
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link" href="/signout">Sign Out</a>
@@ -51,6 +95,10 @@ class Navbar extends Component {
                                 <a className="nav-link" href="/landing">Landing</a>
                             </li>
                         </ul>
+                        <form className="d-flex">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+                            <button className="btn btn-outline-success" type="submit">Search</button>
+                        </form>
                     </div>
                 </div>
             </nav>
