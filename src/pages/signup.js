91ac5cd47d9dab3573ea023b6a872aa3324c8939
Kyle Truong <80617067+ktruong16@users.noreply.github.com>
2023-02-03 16:55:48 -0800

    added school and username input fields
    -added inputs for school and username
    -once user signs up, it will be added to firebase

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,11 +1,14 @@
 import React, { Component, useState, useEffect } from "react";
 import '../App.css';
 //import { useState, useEffect } from 'react';
-import { auth } from '../firebaseConfig'
+import { auth, db } from '../firebaseConfig'
+import { doc, setDoc } from "firebase/firestore"
 import { createUserWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
 
 function SignUp() {
-    // variables to set new email, password, and username
+    // variables to set new email, password, username, and school
+    const [newSchool, setNewSchool] = useState("");
+    const [newUserName, setNewUserName] = useState("");
     const [newEmail, setNewEmail] = useState("");
     const [newPassword, setNewPassword] = useState("");
 
@@ -19,6 +22,13 @@ function SignUp() {
                     .then(() => {
                         console.log("email sent!");
                     });
+                    await setDoc(doc(db, "userInfo", userCredential.user.uid), {
+                        uid: userCredential.user.uid,
+                        school: newSchool,
+                        username: newUserName
+                    })
+                
+
                 console.log(userCredential.user);
                 console.log(auth.currentUser.email);
                 // ...
@@ -34,30 +44,54 @@ function SignUp() {
             <h2>Sign Up</h2>
                 <form onSubmit={signUp}>
                     <div>
+                    <h3>Enter school:
+                        <input
+                            type="text"
+                            placeholder="School..."
+                            value={newSchool}
+                            onChange={(event) => {
+                                setNewSchool(event.target.value);
+                            }}
+                        />          
+                    </h3>
+                    </div>
+                    <div>
+                    <h3>Enter username:
+                        <input
+                            type="text"
+                            placeholder="Username..."
+                            value={newUserName}
+                            onChange={(event) => {
+                                setNewUserName(event.target.value);
+                            }}
+                        />          
+                    </h3>
+                    </div>
+                    <div>
                     <h3>Enter email:
-                    <input
-                        type="email"
-                        placeholder="Email..."
-                        value={newEmail}
-                        onChange={(event) => {
-                            setNewEmail(event.target.value);
-                        }}
-                    />          
+                        <input
+                            type="email"
+                            placeholder="Email..."
+                            value={newEmail}
+                            onChange={(event) => {
+                                setNewEmail(event.target.value);
+                            }}
+                        />          
                     </h3>
-                </div>
-                <div>
-                    <h3> Enter password:
-                    <input
-                        type="password"
-                        placeholder="Password..."
-                        value={newPassword}
-                        onChange={(event) => {
-                            setNewPassword(event.target.value);
-                        }}
-                    />
-                    </h3>
-                </div>
-                <button type="submit"> Sign Up </button>
+                    </div>
+                    <div>
+                        <h3> Enter password:
+                            <input
+                                type="password"
+                                placeholder="Password..."
+                                value={newPassword}
+                                onChange={(event) => {
+                                    setNewPassword(event.target.value);
+                                }}
+                            />
+                        </h3>
+                    </div>
+                    <button type="submit"> Sign Up </button>
                 </form>
         </div>
     );
