acbbb036dc605187c15bc7ba6bbe19fbfa99a337
leilani-cardenas <72056787+leilani-cardenas@users.noreply.github.com>
2023-02-07 18:34:13 -0800

    listingDetails progress

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -8,10 +8,10 @@ class Pagination extends Component {
                     <li class="page-item disabled">
                         <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
                     </li>
-                    <li class="page-item">
+                    <li class="page-item active" aria-current="page">
                         <a class="page-link" href="#">1</a>
                     </li>
-                    <li class="page-item active" aria-current="page">
+                    <li class="page-item">
                         <a class="page-link" href="#">2</a>
                     </li>
                     <li class="page-item">
--- src/pages/listingDetails.js
+++ src/pages/listingDetails.js
@@ -1,36 +1,59 @@
-import React, { useEffect, useState } from "react";
-import {collection, addDoc} from "firebase/firestore";
+import React, { Component} from "react";
+import {doc, getDoc} from "firebase/firestore";
 import { db } from '../firebaseConfig';
 import { useLocation, Link } from "react-router-dom";
 import './listingdetails.css';
+import test from '../images/nintendo-switch-console.png';
 
 // will display all listing details when a listing is clicked on from home page
-const Listingdetails = _ => {
-    const did = window.location.pathname.split("/")[2];
+class Listingdetails extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            seller: ''
+        };
+    }
 
-    document.title="Listing Details";
-    return (
-        <div className="padding container">
-            <form className="d-flex search-form">
-                <input className="form-control me-2 search-input" type="search" placeholder="search here" aria-label="Search"></input>
-                <button className="search-btn btn-outline-success" type="submit">search by filter</button>
-            </form>
-            <div className="row row-style">
-                <div className="col col-style">
-                    <p>item image goes here</p>
-                    <p>compare item link goes here</p>
-                </div>
-                <div className="col col-style">
-                    <div>
-                        <p>listing title</p>
-                        <p>listing seller</p>
-                        <p>listing description</p>
-                        <p>dm user button</p>
+    async componentDidMount(){
+        // grabs document id from url
+        const did = window.location.pathname.split("/")[2];
+        const docRef = doc(db, "marketListings", did);
+        const docSnap = await getDoc(docRef).then((docData)=>{
+            console.log("Doc data: ", docData.data());
+        });
+        this.setState({seller: 'test'});
+        console.log(this.state)
+    }
+
+    render(){
+        const {st} = this.state;
+        console.log('leilani:', );
+
+        document.title="Listing Details";
+        return (
+            <div className="padding container">
+                <form className="d-flex search-form">
+                    <input className="form-control me-2 search-input" type="search" placeholder="search here" aria-label="Search"></input>
+                    <button className="search-btn btn-outline-success" type="submit">search by filter</button>
+                </form>
+                <div className="row row-style">
+                    <div className="col col-style">
+                        <img src={test} class="col-style" alt="..."/>
+                        <p>compare item link goes here</p>
+                    </div>
+                    <div className="col col-style">
+                        <div>
+                            <p>listing price </p>
+                            <p>listing title </p>
+                            <p>listing seller </p>
+                            <p>listing description </p>
+                            <button type="submit">dm user button</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        )
+    }
 }
 
 export default Listingdetails;
