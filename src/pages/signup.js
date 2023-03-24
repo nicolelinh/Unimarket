95f8f23e50f712b311ec15c9d36c00284feab205
nicolesayshi <57279687+nicolesayshi@users.noreply.github.com>
2023-03-23 19:21:18 -0700

    sign-in cleanup

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,5 +1,5 @@
 import React, { Component, useState, useEffect } from "react";
-import '../App.css';
+import '../css/signup.css';
 //import { useState, useEffect } from 'react';
 import { auth, db } from '../firebaseConfig'
 import { doc, setDoc } from "firebase/firestore"
