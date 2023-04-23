8b42343aebd4323b25cf6cad2209e289ff42663d
nicolesayshi <57279687+nicolesayshi@users.noreply.github.com>
2023-04-23 01:33:12 -0700

    update user history and user profile

--- src/pages/userprofile.jsx
+++ src/pages/userprofile.jsx
@@ -1,28 +1,45 @@
-import React, { Component, useState } from "react";
+import React, {useState, useContext } from "react";
+import {doc, getDoc} from "firebase/firestore";
+import { db} from '../firebaseConfig';
 import '../App.css';
 import profilepic from '../assets/profilepic.png';
 import '../css/userprofile.css';
 import profilebackground from '../assets/profilebackground.png';
 import userhistory from '../assets/userhistory.png';
+import {AuthContext} from '../context/AuthContext';
+
 
 
 
     
       
-class UserProfile extends Component {
-        render() {
+function UserProfile () {
         document.title="UserProfile"
+        const [email] = useState(() => {
+            // getting user details from local storage
+            const saved = window.localStorage.getItem('USER_EMAIL');
+            const initialValue = JSON.parse(saved);
+            return initialValue || "";
+          }, []);
         
+        const {currentUser} = useContext(AuthContext);
+        const uid = currentUser.uid;
+        
+        
+
+          
+          
+
         return(
             <main>
                 <section>
                     {/*container holds all information shown to user for their user profile*/}
                     <div className="profilecontainer">
-                        <h1 className="userprofile">UniMarket UserProfile</h1>
+                        <h1 className="userprofile">{email}</h1>
                         <img className="profilepic" src={profilepic} alt="profilepic" id="profilepic"/>
                         <div className="userinformation">
                             {/*this is filler of which user information will show below*/}
-                            <p className="userprofile">user information<br></br>user information</p>
+                            <p className="userprofile">{uid}<br></br>{currentUser.username}</p>
                         </div>
                         {/*this will link to the user history page for the user to navigate towards*/}
                         <button className="userhistory"><a href="/userhistory"><img src={userhistory} alt="userhistory" id="userhistory"></img></a></button>
@@ -33,6 +50,6 @@ class UserProfile extends Component {
         )
 
     }
-}
+
 
 export default UserProfile;
