d4c6069a522ebf286951f57cfe0e1c7374a914b3
nicolesayshi <57279687+nicolesayshi@users.noreply.github.com>
2023-05-06 17:53:46 -0700

    user profile fix

--- src/pages/userprofile.jsx
+++ src/pages/userprofile.jsx
@@ -1,55 +1,69 @@
-import React, {useState, useContext } from "react";
-import {doc, getDoc} from "firebase/firestore";
-import { db} from '../firebaseConfig';
-import '../App.css';
-import profilepic from '../assets/profilepic.png';
-import '../css/userprofile.css';
-import profilebackground from '../assets/profilebackground.png';
-import userhistory from '../assets/userhistory.png';
-import {AuthContext} from '../context/AuthContext';
+import React, { useState, useContext, useEffect } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../firebaseConfig";
+import "../App.css";
+import profilepic from "../assets/profilepic.png";
+import "../css/userprofile.css";
+import profilebackground from "../assets/profilebackground.png";
+import userhistory from "../assets/userhistory.png";
+import { AuthContext } from "../context/AuthContext";
 
+function UserProfile() {
+  document.title = "UserProfile";
+  const [userProfileData, setUserProfileData] = useState(null);
+  const { currentUser } = useContext(AuthContext);
+  const uid = currentUser.uid;
 
+  useEffect(() => {
+    async function getUserProfileData() {
+      try {
+        const docRef = doc(db, "userInfo", uid);
+        const docSnap = await getDoc(docRef);
 
-
-    
-      
-function UserProfile () {
-        document.title="UserProfile"
-        const [email] = useState(() => {
-            // getting user details from local storage
-            const saved = window.localStorage.getItem('USER_EMAIL');
-            const initialValue = JSON.parse(saved);
-            return initialValue || "";
-          }, []);
-        
-        const {currentUser} = useContext(AuthContext);
-        const uid = currentUser.uid;
-        
-        
-
-          
-          
-
-        return(
-            <main>
-                <section>
-                    {/*container holds all information shown to user for their user profile*/}
-                    <div className="profilecontainer">
-                        <h1 className="userprofile">{email}</h1>
-                        <img className="profilepic" src={profilepic} alt="profilepic" id="profilepic"/>
-                        <div className="userinformation">
-                            {/*this is filler of which user information will show below*/}
-                            <p className="userprofile">{uid}<br></br>{currentUser.username}</p>
-                        </div>
-                        {/*this will link to the user history page for the user to navigate towards*/}
-                        <button className="userhistory"><a href="/userhistory"><img src={userhistory} alt="userhistory" id="userhistory"></img></a></button>
-                    </div>
-                    <img className="profilebackground" src={profilebackground} alt="profilebackground" id="profilebackground"/>
-                </section>
-            </main>
-        )
-
+        if (docSnap.exists()) {
+          const data = docSnap.data();
+          console.log("Data from Firebase:", data);
+          setUserProfileData(data);
+        } else {
+          console.log("No such document!");
+        }
+      } catch (error) {
+        console.log("Error getting document:", error);
+      }
     }
 
+    getUserProfileData();
+  }, [uid]);
+
+  console.log("UserProfileData:", userProfileData);
+
+  return (
+    <main>
+      <section>
+        <div className="profilecontainer">
+          <h1 className="userprofile">{userProfileData?.email}</h1>
+          <img className="profilepic" src={profilepic} alt="profilepic" />
+          <div className="userinformation">
+            <p className="userprofile">
+              {userProfileData?.school}
+              <br />
+              {userProfileData?.username}
+            </p>
+          </div>
+          <button className="userhistory">
+            <a href="/userhistory">
+              <img src={userhistory} alt="userhistory" />
+            </a>
+          </button>
+        </div>
+        <img
+          className="profilebackground"
+          src={profilebackground}
+          alt="profilebackground"
+        />
+      </section>
+    </main>
+  );
+}
 
 export default UserProfile;
