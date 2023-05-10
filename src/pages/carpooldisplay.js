8465a28701c5f92ce626f400b0a0c6707ded93b3
nicolesayshi <57279687+nicolesayshi@users.noreply.github.com>
2023-05-10 07:03:44 -0700

    carpool display and request tweak

--- src/pages/carpooldisplay.js
+++ src/pages/carpooldisplay.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import '../App.css';
+import '../css/carpooldisplay.css';
 // importing database link to carpool request listing, as well as the designated folder for inputs. 
 import { db } from '../firebaseConfig';
 import {collection, onSnapshot} from "firebase/firestore";
@@ -24,20 +24,20 @@ const CarpoolDisplay = () => {
   
   return (
 
-  
-    <form  style={{ backgroundColor: '#F8F8F8', padding: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <h2>Available Carpools (For Drivers):</h2>
-      <table style={{ border: '1px solid black' }}>
+    <div className='carpooldisplay-padding'>
+      <h2 className='carpooldisplay-title'>Available Carpools (For Drivers):</h2>
+    <form className='carpooldisplay-form' style={{ backgroundColor: 'transparent', padding: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <table className="carpooldisplay-table" style={{ border: '1px solid #A2A2A2' }}>
         <thead>
-          <tr style={{ border: '1px solid black' }}>
-            <th style={{ border: '1px solid black', padding: '8px' }}>Name</th>
-            <th style={{ border: '1px solid black', padding: '8px' }}>Phone Number</th>
-            <th style={{ border: '1px solid black', padding: '8px' }}>Location From</th>
-            <th style={{ border: '1px solid black', padding: '8px' }}>Location To</th>
-            <th style={{ border: '1px solid black', padding: '8px' }}>Pick-up Time/Date</th>
-            <th style={{ border: '1px solid black', padding: '8px' }}>Est Drive Time</th>
-            <th style={{ border: '1px solid black', padding: '8px' }}>How Many Passengers</th>
-            <th style={{ border: '1px solid black', padding: '8px' }}>Passenger Note</th>
+          <tr className="carpooldisplay-columns" style={{ border: '1px solid #A2A2A2' }}>
+            <th style={{ border: '1px solid #A2A2A2', padding: '8px' }}>Name</th>
+            <th style={{ border: '1px solid #A2A2A2', padding: '8px' }}>Phone Number</th>
+            <th style={{ border: '1px solid #A2A2A2', padding: '8px' }}>Location From</th>
+            <th style={{ border: '1px solid #A2A2A2', padding: '8px' }}>Location To</th>
+            <th style={{ border: '1px solid #A2A2A2', padding: '8px' }}>Pick-up Time/Date</th>
+            <th style={{ border: '1px solid #A2A2A2', padding: '8px' }}>Est Drive Time</th>
+            <th style={{ border: '1px solid #A2A2A2', padding: '8px' }}>How Many Passengers</th>
+            <th style={{ border: '1px solid #A2A2A2', padding: '8px' }}>Passenger Note</th>
           </tr>
         </thead>
         <tbody>
@@ -45,28 +45,33 @@ const CarpoolDisplay = () => {
             //console.log('request:', request);
             return (
               
-              <tr key={index} style={{ border: '1px solid black' }}>
+              <tr className="carpooldisplay-row" key={index} style={{ border: '1px solid #A2A2A2' }}>
                 {console.log(request)}
-                <td style={{ border: '1px solid black', padding: '8px' }}>
-                  <Link to={{pathname:`/carpool/${request.id}`}} >{request.name}</Link>
+                <td style={{ border: '1px solid #A2A2A2', padding: '8px' }}>
+                  <Link className='carpooldisplay-namelinks'to={{pathname:`/carpool/${request.id}`}} >{request.name}</Link>
                   {/* <a href="">{request.name}
                   </a> */}
                 </td>
-                <td style={{ border: '1px solid black', padding: '8px' }}>{request.phone_number}</td>
-                <td style={{ border: '1px solid black', padding: '8px' }}>{request.location_from}</td>
-                <td style={{ border: '1px solid black', padding: '8px' }}>{request.location_to}</td>
-                <td style={{ border: '1px solid black', padding: '8px' }}>{request.pick_up_time_date}</td>
-                <td style={{ border: '1px solid black', padding: '8px' }}>{request.est_drive_time}</td>
-                <td style={{ border: '1px solid black', padding: '8px' }}>{request.how_many_passengers}</td>
-                <td style={{ border: '1px solid black', padding: '8px' }}>{request.passenger_note}</td>
+                <td style={{ border: '1px solid #A2A2A2', padding: '8px' }}>{request.phone_number}</td>
+                <td style={{ border: '1px solid #A2A2A2', padding: '8px' }}>{request.location_from}</td>
+                <td style={{ border: '1px solid #A2A2A2', padding: '8px' }}>{request.location_to}</td>
+                <td style={{ border: '1px solid #A2A2A2', padding: '8px' }}>{request.pick_up_time_date}</td>
+                <td style={{ border: '1px solid #A2A2A2', padding: '8px' }}>{request.est_drive_time}</td>
+                <td style={{ border: '1px solid #A2A2A2', padding: '8px' }}>{request.how_many_passengers}</td>
+                <td style={{ border: '1px solid #A2A2A2', padding: '8px' }}>{request.passenger_note}</td>
               </tr>
             );
           })}
         </tbody>
       </table>
-      <Link to="/carpoolrequest">Redirect to request a carpool for yourself!</Link>
-      <Link to="/location">Or find carpools close to you using our location services!</Link>
+      <div className='carpooldisplay-padding2'>
+      <Link className='carpooldisplay-bottomlinks' to="/carpoolrequest">Redirect to request a carpool for yourself!</Link>
+      <br></br>
+      <Link className='carpooldisplay-bottomlinks' to="/location">Or find carpools close to you using our location services!</Link>
+      </div>
     </form>
+    
+    </div>
   );
 };
 
